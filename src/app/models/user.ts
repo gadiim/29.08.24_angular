0b20e38d64ed51880584ef30fb58303921d58945
Hennadii Melnyk <162840010+gadiim@users.noreply.github.com>
2024-09-04 19:13:50 +0300

    commit

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -3,4 +3,10 @@ export class User {
     name: string = '';
     surname: string = '';
     birth: Date = new Date('YYYY-MM-DD');
+    constructor (id: number, name: string, surname: string, birth: Date ){
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.birth = birth;
+    };
 }
